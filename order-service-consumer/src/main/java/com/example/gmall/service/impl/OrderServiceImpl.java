24e75f5ac88276a3382da03feb678a9a4f83005d
animation <763678968@qq.com>
2019-10-05 19:52:55 +0800

    配置监控中心 提供者 provider.xml

--- order-service-consumer/src/main/java/com/example/gmall/service/impl/OrderServiceImpl.java
+++ order-service-consumer/src/main/java/com/example/gmall/service/impl/OrderServiceImpl.java
@@ -5,15 +5,12 @@ import com.example.gmall.service.OrderService;
 import com.example.gmall.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
-import javax.annotation.Resources;
 import java.util.List;
 
 /**
  * 1.将服务提供者注册到注册中心（暴露服务）
  *      1）导入dubbo依赖(2.6.2)/操作zookeeper的客户端(curator)
- *      2）
+ *      2）配置服务提供者
  * 2.让服务消费者去注册中心订阅服务提供者的服务地址
  */
 @Service
