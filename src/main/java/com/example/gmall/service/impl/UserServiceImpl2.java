187d707651f0418438e74e5c5123276405d44fa8
animation <763678968@qq.com>
2019-10-05 22:36:05 +0800

    重试次数 多版本

--- src/main/java/com/example/gmall/service/impl/UserServiceImpl2.java
+++ src/main/java/com/example/gmall/service/impl/UserServiceImpl2.java
@@ -6,10 +6,10 @@ import com.example.gmall.service.UserService;
 import java.util.Arrays;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl2 implements UserService {
 
     public List<UserAddress> getUserAddressList(String userId) {
-        System.out.println("UserServiceImpl....old.....");
+        System.out.println("UserServiceImpl....new.....");
         UserAddress address1 = new UserAddress(1, "浙江省杭州市浙江大学玉泉校区", "1", "张三", "0571-88888888", "Y");
         UserAddress address2 = new UserAddress(1, "浙江省杭州市浙江大学西溪校区", "1", "李四", "0571-66666666", "Y");
         try {
